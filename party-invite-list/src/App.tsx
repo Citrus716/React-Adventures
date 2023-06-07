c8013a33f7e368caae6fd72170fe9315883e2f7e
lzheng25 <lyzheng@andrew.cmu.edu>
2023-06-07 11:08:10 +0800

    Simple party invite list

---
+++ party-invite-list/src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+import List from "./components/List";
+import AddToList from "./components/AddToList";
+
+export interface IState {
+  people: {
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[];
+}
+
+function App() {
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "Lebron James",
+      age: 36,
+      url: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+      note: "LeFlop never better than MJ.",
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <h1>People invited to my Party</h1>
+      <List people={people}></List>
+      <AddToList people={people} setPeople={setPeople}></AddToList>
+    </div>
+  );
+}
+
+export default App;
